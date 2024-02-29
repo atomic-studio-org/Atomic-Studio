ed32717b28692c88947133929bb8d314534f8591
Tulili <tulilirockz@outlook.com>
2024-02-29 13:47:02 -0300

    feat: GTS release + move some stuff to flatpak

---
+++ config/scripts/rt-kernel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+#rpm-ostree override replace \
+#    --uninstall=kernel \
+#    --uninstall=kernel-core \
+#    --uninstall=kernel-modules \
+#    --uninstall=kernel-headers \
+#    --uninstall=kernel-devel \
+#    --install= \
+#    --install= \
+#    --install= \
+#    --install= \
+#    --install= \
+#    - KERNEL
