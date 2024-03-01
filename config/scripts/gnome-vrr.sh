b0c4415d6777846dfd6cf8f4398aadf53f40d081
Tulili <tulilirockz@outlook.com>
2024-02-29 21:27:27 -0300

    feat: ptyxis, gnome-vrr and yafti

---
+++ config/scripts/gnome-vrr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ARCH=$(uname -i)
+FEDORA_MAJOR_VERSION=$(rpm -E %fedora)
+
+wget https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-gnome-vrr-fedora-"${FEDORA_MAJOR_VERSION}".repo \
+ -O /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
+
+rpm-ostree override replace --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
+
+rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
