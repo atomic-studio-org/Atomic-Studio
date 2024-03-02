eebc86e0f056870930dda1fa6c1042581b569fc7
Tulili <tulilirockz@outlook.com>
2024-03-02 00:01:24 -0300

    fix(rtkernel): remove vanilla kernel from build

--- config/scripts/rt-kernel.sh
+++ config/scripts/rt-kernel.sh
@@ -8,13 +8,28 @@ REPO_FILE_PATH=/etc/yum.repos.d/_copr_ycollet-audinux.repo
 wget https://copr.fedorainfracloud.org/coprs/ycollet/audinux/repo/fedora-${FEDORA_MAJOR_VERSION}/ycollet-audinux-fedora-${FEDORA_MAJOR_VERSION}.repo \
  -O $REPO_FILE_PATH
 
-rpm-ostree override replace --experimental \
-    --uninstall=kernel \
-    --uninstall=kernel-core \
-    --uninstall=kernel-modules \
-    --uninstall=kernel-headers \
-    --uninstall=kernel-devel \
+rpm-ostree cliwrap install-to-root /
+rpm-ostree override replace \
+    --experimental \
+    --remove=kernel \
+    --remove=kernel-core \
+    --remove=kernel-modules \
+    --remove=kernel-headers \
+    --remove=kernel-devel \
     --from repo=copr:copr.fedorainfracloud.org:ycollet:audinux \
-    kernel-rt-mao 
+    kernel-rt-mao
+
+RUN 
+     && \
+    rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:sentry:kernel-fsync \
+        kernel \
+        kernel-core \
+        kernel-modules \
+        kernel-modules-core \
+        kernel-modules-extra \
+        kernel-uki-virt
+
 
 rm -f $REPO_FILE_PATH
