6e8640a59625c0a03045aad582764222e544bb75
Tulili <tulilirockz@outlook.com>
2024-03-04 13:48:14 -0300

    fix: syntax errors + build limit + flake checks

--- config/scripts/zluda.sh
+++ config/scripts/zluda.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-wget "$(curl https://api.github.com/repos/vosen/ZLUDA/releases/latest | jq -r '.assets[] | select(.name| test(".*-linux.tar.gz$")).browser_download_url')" -O /tmp/zluda.tar.gz
+wget "$(curl "https://api.github.com/repos/vosen/ZLUDA/releases/latest" | jq -r '.assets[] | select(.name| test(".*-linux.tar.gz$")).browser_download_url')" \
+  -O /tmp/zluda.tar.gz
 
 mkdir -p /tmp/zluda
 tar --strip-components 1 -xvzf /tmp/zluda.tar.gz -C /tmp/zluda
--- flake.nix
+++ flake.nix
@@ -3,14 +3,13 @@
 
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-
+    nix-pre-commit-hooks.url = "https://github.com/cachix/pre-commit-hooks.nix/tarball/master";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
   };
 
   
-  outputs = { self, flake-schemas, nixpkgs }:
+  outputs = { self, flake-schemas, nixpkgs, nix-pre-commit-hooks }:
     let
-      
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
         pkgs = import nixpkgs { inherit system; };
@@ -19,6 +18,24 @@
       });
     in {
       schemas = flake-schemas.schemas;
+
+      checks = forEachSupportedSystem ({system, pkgs,...}: {
+        pre-commit-check = nix-pre-commit-hooks.lib.${system}.run {
+          src = pkgs.lib.cleanSource ./.;
+          # If your hooks are intrusive, avoid running on each commit with a default_states like this:
+          # default_stages = ["manual" "push"];
+          hooks = {
+            elm-format.enable = true;
+            ormolu.enable = true;
+            shellcheck.enable = true;
+          };
+ 
+          # Some hooks offer custom settings that affect how they execute
+          settings = {
+            ormolu.defaultExtensions = [ "lhs" "hs" ];
+          };          
+        };
+      });
 
       packages = forEachSupportedSystem ({ system, pkgs, lib, ... }: {
         # replace with pkgs.bluebuild whenever flake is done
@@ -33,10 +50,9 @@
           def "main" [inputFile: string, outputFolder: string, outputName: string, extension: string, ...rest] {
           mkdir $outputFolder
           [ 16 32 64 128 256 ] | par-each { 
-            |size| 
-              do { ${lib.getExe pkgs.ffmpeg} -y -i $inputFile -vf $"scale=($size):($size)" $"($outputFolder)/($outputName)-($size)x($size).($extension)" } & 
+            |size| do { ${lib.getExe pkgs.ffmpeg} -y -i $inputFile -vf $"scale=($size):($size)" $"($outputFolder)/($outputName)-($size)x($size).($extension)" } & 
           }
-          ${lib.getExe pkgs.ffmpeg} -y -i $inputFile $"($outputFolder)/($outputName).($extension)"
+          ${lib.getExe pkgs.ffmpeg} -y -i $inputFile $"($outputFolder)/($outputName).($extension)" &
         }
         '';
       });
