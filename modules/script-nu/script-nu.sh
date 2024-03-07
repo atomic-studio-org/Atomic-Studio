15de66bd1b16deef57a158c2b4aaf2b616f203a3
Tulili <tulilirockz@outlook.com>
2024-03-06 21:22:30 -0300

    fix: typo!

--- modules/script-nu/script-nu.sh
+++ modules/script-nu/script-nu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-NU_LOG_LEVEL=DEBUG nu --stdin /tmp/config/modules/script-nu/nushell-launcher.nu $@
+NU_LOG_LEVEL=DEBUG nu --stdin /tmp/modules/script-nu/nushell-launcher.nu $@
