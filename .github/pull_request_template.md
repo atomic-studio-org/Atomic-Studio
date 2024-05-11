677e7ff8b927e2a1186ffed612c0fababd2ff880
Tulili <tulilirockz@outlook.com>
2024-05-11 20:32:51 -0300

    feat: cleanup repo + add local building (#81)

--- .github/pull_request_template.md
+++
@@ -1,5 +0,0 @@
-<!---
-## Thank you for contributing to the Atomic Studio project!
-
-Please [read the Contributor's Guide](https://github.com/atomic-studio-org/.github/CONTRIBUTING.md) before submitting a pull request.
--->
--- flake.nix
+++ flake.nix
@@ -2,71 +2,57 @@
   description = "Atomic Studio development utility flake";
 
   inputs = {
-    utility-flake.url = "github:atomic-studio-org/Utility-Flake-Library";
-    bluebuild.url = "https://flakehub.com/f/blue-build/cli/0.8.2.tar.gz";
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    pkl.url = "github:tulilirockz/apple-pkl-flake";
+    blue-build.url = "github:blue-build/cli";
   };
 
   outputs =
-    { self
-    , flake-schemas
-    , nixpkgs
-    , bluebuild
-    , utility-flake
-    ,
-    }:
+    { self, nixpkgs, pkl, blue-build }@inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      schemas = flake-schemas.schemas;
-
-      checks = forEachSupportedSystem ({ pkgs }: {
-        inherit (utility-flake.checks.${pkgs.system}) pre-commit-check;
-      });
-
-      packages = forEachSupportedSystem ({ pkgs
-                                         ,
-                                         }: {
-        inherit (utility-flake.packages.${pkgs.system}) cosign-generate;
-        inherit (utility-flake.packages.${pkgs.system}) generate-sbkey;
-
-        build-image = pkgs.writers.writeNuBin "build-image" ''
-          def "main" [--prefix (-p): string, ...recipes: string] {
-            mut recipe_prefix: string = ""
-            if $prefix != null {
-              $recipe_prefix = $"($prefix)\/"
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          generate-logo = pkgs.writers.writeNuBin "generate-logo" ''
+            def "main" [inputFile: string, outputFolder: string, outputName: string, extension: string, ...rest] {
+              mkdir $outputFolder
+              [ 16 32 64 128 256 ] | par-each {
+                |size| do { ${pkgs.lib.getExe pkgs.ffmpeg} -y -i $inputFile -vf $"scale=($size):($size)" $"($outputFolder)/($outputName)-($size)x($size).($extension)" }
+              }
+              ${pkgs.lib.getExe pkgs.ffmpeg} -y -i $inputFile $"($outputFolder)/($outputName).($extension)" &
             }
-            let final_prefix = $recipe_prefix
+          '';
+        }
+      );
 
-            $recipes | par-each { |recipe| do {${bluebuild.packages.${pkgs.system}.bluebuild}/bin/bluebuild build $"config/recipes/($final_prefix)($recipe).yml" } out> $"/tmp/bluebuild-($recipe).log" & }
-          }
-        '';
+      formatter = forEachSupportedSystem ({ pkgs }: pkgs.nixfmt-rfc-style);
 
-        # This script is surprisingly fast!
-        generate-logo = pkgs.writers.writeNuBin "generate-logo" ''
-          def "main" [inputFile: string, outputFolder: string, outputName: string, extension: string, ...rest] {
-            mkdir $outputFolder
-            [ 16 32 64 128 256 ] | par-each {
-              |size| do { ${pkgs.lib.getExe pkgs.ffmpeg} -y -i $inputFile -vf $"scale=($size):($size)" $"($outputFolder)/($outputName)-($size)x($size).($extension)" }
-            }
-            ${pkgs.lib.getExe pkgs.ffmpeg} -y -i $inputFile $"($outputFolder)/($outputName).($extension)" &
-          }
-        '';
-      });
-
-      formatter = forEachSupportedSystem ({ pkgs }: pkgs.nixpkgs-fmt);
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ bluebuild.packages.${system}.bluebuild nushell git jq yq jsonnet openssl ];
-        };
-      });
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nushell
+              git
+              jq
+              yq
+              jsonnet
+              openssl
+              inputs.pkl.packages.${pkgs.system}.pkl
+              inputs.blue-build.packages.${pkgs.system}.default
+            ];
+          };
+        }
+      );
     };
 }
